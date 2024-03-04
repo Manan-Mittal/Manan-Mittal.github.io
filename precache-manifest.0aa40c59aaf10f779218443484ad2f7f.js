481586f1cbb0bd73391e130b4213fabd66d72f2e
Manan Mittal <manan.mittal2020@gmail.com>
2024-03-04 17:12:06 -0500

    Updates

---
+++ precache-manifest.0aa40c59aaf10f779218443484ad2f7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9814e39100749170c6947f41d0d64b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a780a4588b2beacbe091",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "6525bf8fc4385bf3c7a2",
+    "url": "/static/js/2.bcc28277.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.bcc28277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "a780a4588b2beacbe091",
+    "url": "/static/js/main.1fcc0c14.chunk.js"
+  },
+  {
+    "revision": "6521385c839cadf3457c",
+    "url": "/static/js/runtime-main.5ef167bd.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Manan-Mittal.github.io/precache-manifest.c4534f29c85a2a7d8c6c50603e101d64.js"
+  "/precache-manifest.0aa40c59aaf10f779218443484ad2f7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Manan-Mittal.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
